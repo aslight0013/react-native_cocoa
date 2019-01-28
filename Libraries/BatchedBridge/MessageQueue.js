ptmt/react-native-macos

commit 906f5de81e1978787223dffe610293b3401b42b5

fix: detect circular refs in bridge messages
also, stringify invalid messages for easier debugging

Libraries/BatchedBridge/MessageQueue.js
@@ -219,35 +219,63 @@ class MessageQueue {
       // folly-convertible.  As a special case, if a prop value is a
       // function it is permitted here, and special-cased in the
       // conversion.
-      const isValidArgument = val => {
+      const seen = [];
+      const isValid = (function validate(val) {
         const t = typeof val;
         if (
           t === 'undefined' ||
-          t === 'null' ||
           t === 'boolean' ||
-          t === 'number' ||
           t === 'string'
         ) {
           return true;
         }
+        if (t === 'number') {
+          return isFinite(val);
+        }
         if (t === 'function' || t !== 'object') {
           return false;
         }
+        if (seen.indexOf(val) >= 0) {
+          return false;
+        }
+        seen.push(val);
         if (Array.isArray(val)) {
-          return val.every(isValidArgument);
+          return val.every(validate);
         }
         for (const k in val) {
-          if (typeof val[k] !== 'function' && !isValidArgument(val[k])) {
+          if (!validate(val[k])) {
             return false;
           }
         }
         return true;
+      })(params);
+
+      // Stringify the invalid params for easier debugging.
+      const stringify = val => {
+        const seen = [];
+        return JSON.stringify(params, (key, val) => {
+          const t = typeof val;
+          if (t === 'function') {
+            return '[Function' + (val.name ? ': ' + val.name : '') + ']';
+          }
+          if (t === 'number' && !isFinite(val)) {
+            return '[Number: ' + val + ']';
+          }
+          if (t !== 'object') {
+            return val;
+          }
+          if (seen.indexOf(val) >= 0) {
+            return '[Circular]';
+          }
+          seen.push(val);
+          return val;
+        });
       };
 
       invariant(
-        isValidArgument(params),
-        '%s is not usable as a native method argument',
-        params,
+        isValid,
+        'Argument to native method cannot be serialized: %s',
+        isValid || stringify(params),
       );
 
       // The params object should not be mutated after being queued

aslight0013/react-native_cocoa

commit 422df6e6cfbbb8d850be0b803565e70ec0ec12d1

fix: account for null in bridge messages
This regression was introduced by: 906f5de81e1978787223dffe610293b3401b42b5

Libraries/BatchedBridge/MessageQueue.js
@@ -221,12 +221,11 @@ class MessageQueue {
       // conversion.
       const seen = [];
       const isValid = (function validate(val) {
+        if (val == null) {
+          return true;
+        }
         const t = typeof val;
-        if (
-          t === 'undefined' ||
-          t === 'boolean' ||
-          t === 'string'
-        ) {
+        if (t === 'boolean' || t === 'string') {
           return true;
         }
         if (t === 'number') {
@@ -254,6 +253,9 @@ class MessageQueue {
       const stringify = val => {
         const seen = [];
         return JSON.stringify(params, (key, val) => {
+          if (val == null) {
+            return val;
+          }
           const t = typeof val;
           if (t === 'function') {
             return '[Function' + (val.name ? ': ' + val.name : '') + ']';